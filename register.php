b796fcfd126696df29f8a18c388e26fd9f220fee
vaiola3 <newillianpereira@gmail.com>
2019-11-01 23:16:16 -0300

    tela simples de cadastro

---
+++ register.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="semantic/dist/semantic.min.js"></script>
+        <title>projeto_rosaParks</title>
+    </head>
+
+    <body class="masthead">
+		<div class="ui container" style="padding-top: 10%">
+			<div class="ui two column middle center aligned grid">
+				<div class="sixteen wide mobile eight wide tablet eight wide computer column">
+					<form class="ui form">
+                        <div class="field">
+                            <h1>Cadastro</h1>
+                        </div>
+                        <div class="field">
+                            <div class="ui large icon input">
+                                <input type="text" name="first-name" placeholder="Nome Completo">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui large icon input">
+                                <input type="text" name="last-name" placeholder="Endereço de Email">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui large icon input">
+                                <input type="text" name="last-name" placeholder="Usuário">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="two fields">
+                                <div class="field">
+                                    <div class="ui large icon input">
+                                        <input type="password" name="last-name" placeholder="Senha">
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <div class="ui large icon input">
+                                        <input type="password" name="last-name" placeholder="Repita a Senha">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="ui large purple button" type="submit">Cadastrar</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
