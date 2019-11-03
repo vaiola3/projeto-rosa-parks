09d88abb6f60f30fc88d1a52ca74f4941c8c9fd3
vaiola3 <newillianpereira@gmail.com>
2019-11-02 21:55:47 -0300

    incluindo arquivos em arquitetura MVC

--- index.php
+++ index.php
@@ -1,40 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-    <style>
-        .masthead {
-            background-image: url('img/bckground.jpg') !important;
-            background-size: auto !important;
-        }
-    </style>
+<?php
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
-        <script src="semantic/dist/semantic.min.js"></script>
-        <title>projeto_rosaParks</title>
-    </head>
+    require('app/core/core.php');
+    require('app/controller/HomeController.php');
+    require('app/controller/RegisterController.php');
+    require('app/controller/ErrorController.php');
 
-    <body class="masthead">
-		<div class="ui container" style="padding-top: 17%">
-			<div class="ui four column middle center aligned grid">
-				<div class="sixteen wide mobile eight wide tablet four wide computer column">
-					<form class="ui form">
-					  <div class="field">
-                      <div class="ui large icon input">
-                        <input type="text" name="first-name" placeholder="Login">
-                        </div>
-					  </div>
-					  <div class="field">
-                        <div class="ui large icon input">
-                        <input type="password" name="last-name" placeholder="Senha">
-                        </div>
-					  </div>
-					  <button class="ui large purple button" type="submit">Enviar</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</body>
-</html>
+    $template = file_get_contents('app/template/estrutura.html');
+
+    ob_start(); //  begin
+        $core = new Core;
+        $core->start( $_GET );
+
+        $saida = ob_get_contents();
+    ob_clean(); //  end
+
+    $readyTemplate = str_replace( '{{CONTENT}}', $saida, $template );
+
+    echo $readyTemplate;
