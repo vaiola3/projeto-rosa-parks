408f3e2b3613583bfde183b819b2b4c07091449d
vaiola3 <newillianpereira@gmail.com>
2020-06-07 03:07:11 -0300

    revisar arquivo Core.php

---
+++ app/core/Core.php
@@ -0,0 +1,45 @@
+<?php 
+
+	class Core {
+		private $proxController;
+		private function verificarSolicitacaoRegistro() {
+			$parametro = $this->obterParametro('getNewRegister');
+            return ($parametro == "true");
+		}
+
+		private function verificarSolicitacaoLogout() {
+			$parametro = $this->obterParametro('LogOutSession');
+			if($parametro == "true"){
+				session_destroy();
+                $_SESSION = [];
+			}
+		}
+
+		public function carregarConteudo() {
+
+			$this->verificarSolicitacaoLogout();
+
+			$solicitouCadastro = $this->verificarSolicitacaoRegistro();
+
+			if($solicitouCadastro){
+				(new RegistroController)->start();
+			} else {
+				(new LoginController)->start();
+			}
+		}
+
+		private function obterParametro($parametro) {
+			$resultado = filter_input( 
+                INPUT_POST, 
+                $parametro, 
+                FILTER_SANITIZE_STRING,
+                FILTER_FLAG_NO_ENCODE_QUOTES
+			);
+
+			return $resultado;
+		}
+
+		#	getters / setters
+	}
+
+ ?>
